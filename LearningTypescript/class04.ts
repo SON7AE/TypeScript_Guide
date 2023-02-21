2e01b5e01b931f09b2c613d7090b5186dfe5a26d
SON7AE <bobsot0225@gmail.com>
2023-02-22 05:16:40 +0900

    러닝타입스크립트 - 4장 : 객체

--- LearningTypescript/class04.ts
+++ LearningTypescript/class04.ts
@@ -144,3 +144,77 @@ const poemMissMatch: Poem = {
 // 변수에 여러 객체 타입 중 하나가 될 수 있는 초깃값이 주어지면 타입스크립트는 해당 타입을 객체 타입 유니언으로 유추한다.
 // 유니언 타입은 가능한 각 객체 타입을 구성하고 있는 요소를 모두 가질 수 있다.
 // 객체 타입에 정의된 각각의 가능한 속성은 비록 초깃값이 없는 선택적(?)타입이지만 각 객체 타입의 구성 요소로 주어진다.
+
+// 4.3.2 명시된 객체 타입 유니언
+// 객체 타입의 조합을 명시하면 객체 타입을 더 명확히 정의할 수 있다.
+// 코드를 조금 더 작성해야 하지만 객체 타입을 더 많이 제어할 수 있는 이점이 있다.
+// 특히 값의 타입이 객체 타입으로 구성된 유니언이라면 타입스크립트의 타입 시스템은 이런 모든 유니언 타입에 존재하는 속성에 대한 접근만 허용한다.
+
+type PoemWithPages = {
+    name: string
+    pages: number
+}
+type PoemWithRhymes = {
+    name: string
+    rhymes: boolean
+}
+type Poem = PoemWithPages | PoemWithRhymes
+
+const poem: Poem = Math.random() > 0.5 ? { name: 'The Double Image', pages: 7 } : { name: 'Her Kind', rhymes: true }
+poem.name // OK
+poem.pages // 공통적으로 pages 속성이 없기 때문에 에러 표시
+poem.rhymes // 공통ㅇ적으로 rhymes 속성이 없기 때문에 에러 표시
+
+// 잠재적으로 존재하지 않는 객체의 멤버에 대한 접근을 제한하면 코드의 안전을 지킬 수 있다.
+// 값이 여러 타입 중 하나일 경우, 모든 타입에 존재하지 않는 속성이 객체에 존재할 거라 보장할 수 없다.
+// 리터럴 타입이나 원시 타입 모두, 혹은 둘 중 하나로 이루어진 유니언 타입에서 모든 타입에 존재하지 않은 속성에 접근하기 위해 타입을 좁혀야 하는 것처럼
+// 객체 타입 유니언도 타입을 좁혀아 한다.
+
+// 4.3.3 객체 타입 내로잉
+// 타입 검사기가 유니언 타입 값에 특정 속성이 포함된 경우에만 코드 영역을 실행할 수 있음을 알게 되면,
+// 값의 타입을 해당 속성을 포함하는 구성 요소로만 좁힌다.
+// 즉, 코드에서 객체의 형태를 확인하고 타입 내로잉이 객체에 적용된다.
+
+// 명시적으로 입력된 Poem 예제를 계속 살펴보면, poem의 pages가 타입스크립트의 타입 가드 역할을 하여 PoemWithPages임을 나타내는지 확인한다.
+// 만일 Poem이 PoemWithPages가 아니라면 PoemWithPhymes이어야 한다.
+
+if ('pages' in poem) {
+    poem.pages // OK: poem은 PoemWithPages로 좁혀진다.
+} else {
+    poem.rhymes // OK: poem은 PoemWithRhymes로 좁혀진다.
+}
+
+// 그러나 타입스크립트는 if (poem.pages)와 같은 형식으로 참 거짓 여부를 확인하는 것을 허용하지 않는다.
+// 존재하지 않는 객체의 속성에 접근하려고 시도하면 타입 가드처럼 작동하는 방식으로 사용되더라도 타입 오류로 간주된다.
+
+// 4.3.4 판변될 유니언
+// 자바스크립트와 타입스크립트에서 유니언 타입으로 된 객체의 또 다른 인기 있는 형태는 객체의 속성이 객체의 형태를 나타내도록 하는 것이다.
+// 이러한 타입 형태를 '판별된 유니언'이라 부르고, 객체의 타입을 가리키는 속성이 '판별값'이다.
+// 타입스크립트는 코드에서 판별 속성을 사용해 타입 내로잉을 수행한다.
+// 타입 내로잉 없이는 값에 존재하는 속성을 보장할 수 없다.
+
+type PoemWithPages2 = {
+    name: string
+    pages: number
+    type: 'pages'
+}
+type PoemWithRhymes2 = {
+    name: string
+    rhymes: boolean
+    type: 'rhymes'
+}
+type Poem2 = PoemWithPages2 | PoemWithRhymes2
+
+const poem2: Poem2 = Math.random() > 0.5 ? { name: 'The Double Image', pages: 7, type: 'pages' } : { name: 'Her Kind', rhymes: true, type: 'rhymes' }
+
+if (poem2.type === 'pages') {
+    console.log(`It's got pages: ${poem2.pages}`)
+} else {
+    console.log(`It rhymes: ${poem2.rhymes}`)
+}
+
+poem2.type // 타입: 'pages' | 'rhymes'
+
+// 판별된 유니언은 우아한 자바스크립트 패턴과 타입스크립트의 타입 내로잉을 아름답게 결합한다.
+
+// 4.4 교차 타입
