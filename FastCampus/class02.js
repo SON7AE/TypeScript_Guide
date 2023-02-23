d4b8ad308599b66fa3cfa9769ba39c7845476d98
SON7AE <bobsot0225@gmail.com>
2023-02-24 07:11:33 +0900

    패스트캠퍼스 - 부록 강의 요약

---
+++ FastCampus/class02.js
@@ -0,0 +1,17 @@
+// 2. 문법-값(Value)
+
+// 기본 데이터 타입
+// - Boolean
+// - Null
+// - Undefined
+// - Number
+// - String
+// - Symbol
+// 별도로 Object가 있다.
+
+const Color = {
+    Red: 1,
+    Blue: 2,
+    Green: 3,
+}
+Color.Red
